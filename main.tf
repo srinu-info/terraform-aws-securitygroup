59438060f114f9ea95a5f09f5b525cc33d7c46b8
srinu-info <sriuvasuluyaram@gmail.com>
2026-06-13 17:11:48 +0530

    terraform resourced

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ resource "aws_security_group" "main" {
     description = var.sg_description
     vpc_id = var.vpc_id
 
-    egress = {
-        from_port=0
-        to_port=0
-        protocol=-1
-        cidr_blocks=["0.0.0.0/0"]
-    }
+   egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
     tags=merge(
         var.sg_tags,
         local.common_tags,
