a9908e9de60458dbb773b2ae39be31c87157cb1a
srinu-info <sriuvasuluyaram@gmail.com>
2026-06-13 17:01:56 +0530

    terraform resourced

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "main" {
     vpc_id = var.vpc_id
 
     egress = {
-        frrm_port=0
+        from_port=0
         to_port=0
         protocal=-1
         cidr_blocks=["0.0.0.0/0"]
